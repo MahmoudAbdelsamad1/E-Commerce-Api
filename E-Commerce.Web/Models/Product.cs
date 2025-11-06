641d55f03267d796e8544af490f9410416322b5a
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2025-11-06 21:22:58 +0200

    Add project files.

---
+++ E-Commerce.Web/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ E-Commerce.Web/Program.cs
@@ -0,0 +1,41 @@
+
+namespace E_Commerce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            #endregion
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            #region Configure the HTTP request pipeline. // MidelWare 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers(); 
+            #endregion
+
+            app.Run();
+        }
+    }
+}
